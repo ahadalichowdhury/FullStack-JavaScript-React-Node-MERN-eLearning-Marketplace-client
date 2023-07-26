da8c38cada083c6b332e12755738e01352b62280
S. M. Ahad Ali Chowdhury <77457836+ahadalichowdhury@users.noreply.github.com>
2023-07-27 00:47:15 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require("next-pwa")({
     dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
+    register: true, 
 });
 
 const nextConfig = withPWA({
+    // next config
     reactStrictMode: true,
 });
 module.exports = nextConfig;
