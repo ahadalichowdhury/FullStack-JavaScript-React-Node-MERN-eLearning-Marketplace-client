d385c9546b24aef16e797182fff33b62a8d84ff0
ahadalichowdhury <smahadalichowdhury@gmail.com>
2023-08-02 22:19:49 +0600

    live server add

--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -130,7 +130,7 @@ const singleCourse = ({course}) => {
 }
 
 export async function getServerSideProps ({query}){
-    const {data} = await axios.get(`http://localhost:8000/api/v1/course/${query.slug}`);
+    const {data} = await axios.get(`http://128.199.30.226/api/v1/course/${query.slug}`);
     return {
         props: {
             course: data
